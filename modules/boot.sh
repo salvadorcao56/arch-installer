f48365c9322edede730055dae4dc3b17c765e40f
Salva <salvadorcao56@gmail.com>
2026-05-31 20:10:30 +0200

    fix: replicate official archinstall GRUB config - uncomment GRUB_ENABLE_CRYPTODISK, remove redundant efivars mount

--- modules/boot.sh
+++ modules/boot.sh
@@ -9,10 +9,10 @@ install_bootloader() {
 
 arch-chroot /mnt /bin/bash <<EOF
 sed -i 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID=$CRYPT_UUID:cryptroot"/' /etc/default/grub
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
+sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
 EOF
     fi
 
-    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable || true
+    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
     arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg || true
 }
